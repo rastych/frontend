9ad915ae7f3ec62b3f8fe01407721eeb88cd7764
rastych <rastych@Yuriys-MacBook-Pro-2.local>
2016-11-30 17:09:12 +0200

    lesson 10 hw rc1

--- lesson10/less10.js
+++ lesson10/less10.js
@@ -4,59 +4,59 @@
 'use strict';
 (function () {
     var lighter = document.querySelector('.lighter'),
-        topDisplay = lighter.querySelector('.top'),
-        bottomDisplay = lighter.querySelector('.bottom'),
+        topDisplay = document.querySelector('.top'),
+        bottomDisplay = document.querySelector('.bottom'),
         btns = lighter.querySelectorAll('.btn'),
         operator = [0],
-        a,
         i;
-
+        function calc(a, b, operator) {
+            debugger;
+            a = +a;
+            b = +b;
+            switch(operator) {
+                case "+":
+                    return a + b;
+                    break;
+                case "-":
+                    return a - b;
+                    break;
+                case "*":
+                    return a * b;
+                    break;
+                case "/":
+                    return a / b;
+                    break;
+                case "%":
+                    return a * 0.01 * b;
+                    break;
+            }
+        }
     for(i = 0; i < btns.length; i++) {
         btns[i].addEventListener('click', function () {
             bottomDisplay.value += this.innerHTML;
+            console.log(bottomDisplay.value);
             if(this.innerHTML === "+" || this.innerHTML === "-" || this.innerHTML === "*" || this.innerHTML === "/" || this.innerHTML === "%") { //отрубаю ввод операнда
                 operator.push(this.innerHTML); //знак операции сохраняю;
                 if (topDisplay.value === '') {
                     topDisplay.value = bottomDisplay.value;
                     bottomDisplay.value = '';
                 } else {
+                    debugger;
                     operator.shift();
-                    console.log(operator);
                     topDisplay.value = calc(parseFloat(topDisplay.value), parseFloat(bottomDisplay.value), operator[0]);
                     bottomDisplay.value = '';
                 }
             }
             else if (this.innerHTML === "=") {
                 operator.shift();
-                console.log(operator);
                 bottomDisplay.value = calc(parseFloat(topDisplay.value), parseFloat(bottomDisplay.value), operator[0]);
                 topDisplay.value = '';
-                console.log(operator);
             }
-            else if (this.innerHTML === "C") {
+            else if (this.innerHTML === "AC") {
                 topDisplay.value = '';
                 bottomDisplay.value = '';
-                operator = [];
+                operator = [0];
             }
         });
-        function calc(a, input, operator) {
-            switch(operator) {
-                case "+":
-                    return a + input;
-                    break;
-                case "-":
-                    return a - input;
-                    break;
-                case "*":
-                    return a * input;
-                    break;
-                case "/":
-                    return a / input;
-                    break;
-                case "%":
-                    return a * 0.01 * input;
-                    break;
-            }
-        }
     }
 })();
